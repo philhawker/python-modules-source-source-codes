eb205ee4e489123da44038545ba44910e1ab31fa
Phil Hawker <philhawker1211@gmail.com>
2020-01-20 12:08:31 -0700

    list of nested dictionaries

---
+++ working-with-lists-if-nested-dictionaries.py
@@ -0,0 +1,21 @@
+teams = [
+  {
+    'astros': {
+      '2B': 'Altuve',
+      'SS': 'Correa',
+      '3B': 'Bregman',
+    }
+  },
+  {'angels': {
+      'OF': 'Trout',
+      'DH': 'Pujos',
+    }
+  }
+]
+
+print(teams)
+print(teams[0])   #grab just astros in the same way as usual by using index position
+
+angels = teams[1].get('angels', 'Team not found')
+
+print(angels)
