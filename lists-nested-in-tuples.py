3f469ad06f9ba92565f91672ad0502637a3152cb
Phil Hawker <philhawker1211@gmail.com>
2020-01-20 16:09:43 -0700

    lists nested in tuples

---
+++ lists-nested-in-tuples.py
@@ -0,0 +1,8 @@
+post = ('Python Basics', 'Intro Guide To Python', 'Cool content')
+
+tags = ['python', 'coding', 'tutorial']
+
+post += (tags,)
+
+print(post)
+print(post[-1][1])   #tags can now be traversered to access a specific string
