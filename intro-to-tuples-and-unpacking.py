a2a75b19d630a05afe9f830b68da788b82e4a944
Phil Hawker <philhawker1211@gmail.com>
2020-01-20 15:52:23 -0700

    uploaded into to tuples.py

---
+++ intro-to-tuples-and-unpacking.py
@@ -0,0 +1,15 @@
+# list: []
+# dictionary: {}
+# tuple: ()
+
+# tuple: immutable
+# list: mutable
+
+post = ('Python Basics', 'Intro Guide To Python', 'Cool content')
+
+title, sub_heading, content = post   # this is called UNPACKING
+
+
+print(title,)
+print(sub_heading)
+print(content)
