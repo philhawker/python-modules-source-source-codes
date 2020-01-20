c6c25b4f905c6bf3ef70f9ed141e7d0acd6fe3d2
Phil Hawker <philhawker1211@gmail.com>
2020-01-20 16:14:10 -0700

    slicing tuples

---
+++ slices-in-tuples.py
@@ -0,0 +1,5 @@
+post = ('Python Basics', 'Intro Guide To Python', 'Cool content', 'published')
+
+print(post[:2])   # This slices UP TO index 2
+
+print(post[1::2])   #using :: will skip every two index items 
