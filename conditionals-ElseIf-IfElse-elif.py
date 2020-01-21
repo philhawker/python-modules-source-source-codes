df08c75ed9c8879ea96775c4cd3cd2d6fac67c65
Phil Hawker <philhawker1211@gmail.com>
2020-01-21 14:46:37 -0700

    updated file name

---
+++ conditionals-ElseIf-IfElse-elif.py
@@ -0,0 +1,10 @@
+age = 101
+
+
+# This is a basic if/else statement
+if age < 25:
+  print(f'Sorry, you need to be 25 years old')
+elif age > 100:
+  print(f'Sorry, {age} is too old to rent a car')
+else:
+  print(f'You are good to go, {age} fits in the range to rent a car.')
