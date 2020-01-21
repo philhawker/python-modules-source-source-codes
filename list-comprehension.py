112acabab83fed52571c5fa9ad6efe1d38688234
Phil Hawker <philhawker1211@gmail.com>
2020-01-21 13:42:05 -0700

    added comment notes

--- list-comprehension.py
+++ list-comprehension.py
@@ -4,6 +4,6 @@ cubed_nums = []
 # for num in num_list:
 #   cubed_nums.append(num ** 3)
 
-cubed_nums = [num ** 3 for num in num_list]   #first item in [] is the desired action
+cubed_nums = [num ** 3 for num in num_list]   #first item in [] is the desired action. The name of"num" can be anything i want
 
 print(cubed_nums)
