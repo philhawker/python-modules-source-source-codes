28cf93e1362c604aba1b2b0ea08696582b174d82
Phil Hawker <philhawker1211@gmail.com>
2020-01-21 16:08:36 -0700

    added functions

--- functions-basic-syntax.py
+++ functions-basic-syntax.py
@@ -7,3 +7,28 @@ full_name('Phil', 'Hawker')
 full_name('Rick', 'Hawker')
 full_name('Cristi', 'Benedict')
   
+  
+def auth(email, password):
+  if email == 'philhawker1211@gmail.com' and password == 'secret':
+    print('Authorized')
+  else:
+    print('Not authorized')
+
+
+auth('philhawker1211@gmail.com', 'secret')
+
+
+
+def hundred():
+  for num in range(1, 101):
+    print(num)
+
+
+hundred()
+
+def counter(max_value):
+  for num in range(1, max_value):
+    print(num)
+    
+
+counter(51)
