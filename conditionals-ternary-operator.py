7ef21fdc14c6fbeb66514b6aca96ed22fd36f4c3
Phil Hawker <philhawker1211@gmail.com>
2020-01-21 15:09:03 -0700

    initial commit

---
+++ conditionals-ternary-operator.py
@@ -0,0 +1,18 @@
+role = 'admin'
+
+
+auth = 'can access' if role == 'admin' else 'cannot access'
+
+print(auth)
+
+
+#this is the same as above, but the long way
+"""
+ if role == 'admin':
+   auth = 'can access'
+ else:
+   auth = 'cannot access'
+
+print(auth)
+
+"""
