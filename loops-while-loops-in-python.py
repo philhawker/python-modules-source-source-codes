ee973021548a2bc43afcbc816d3075300d2ff885
Phil Hawker <philhawker1211@gmail.com>
2020-01-21 12:16:56 -0700

    added comments

--- loops-while-loops-in-python.py
+++ loops-while-loops-in-python.py
@@ -4,3 +4,17 @@ nums = list(range(1, 100))
 
 while len(nums) > 0:  # "while the length of nums is greater than 0"
   print(nums.pop())   
+
+
+def guessing_game():
+  while True:       # While True is useful when you dont know how many times you want your code to loop
+    print('what is your guess?')
+    guess = input()
+
+    if guess == '42':
+      print('your guess is correct')
+      return False     # False is to stop the loop
+    else:
+      print(f'no, {guess} is not the answer, please try again')
+      
+      
