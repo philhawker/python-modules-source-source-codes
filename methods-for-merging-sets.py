035af6bc4a327755298bcbbabda3b19699e71027
Phil Hawker <philhawker1211@gmail.com>
2020-01-21 10:19:00 -0700

    added methods

--- methods-for-merging-sets.py
+++ methods-for-merging-sets.py
@@ -17,3 +17,19 @@ tags_two = {
 merged_tags = tags_one | tags_two   # will combine both sets and NOT repeat duplicates
 
 print(merged_tags)
+
+
+# tags in tags_one but NOT in tags_two
+
+exclusive_to_tags_two = tags_two - tags_one   #this returns tags that only exist in tags_one
+exclusive_to_tags_one = tags_one - tags_two   #this returns tags that only exist in tags_two
+
+print(exclusive_to_tags_one)
+print(exclusive_to_tags_two)
+
+
+# tags found in both/shared in both sets
+
+universal_tags = tags_one & tags_two
+
+print(universal_tags)
