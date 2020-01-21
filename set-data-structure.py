5e254830cdd4e6cb7e7e7e83d5e6386204ac4af7
Phil Hawker <philhawker1211@gmail.com>
2020-01-21 10:04:41 -0700

    set data structure

---
+++ set-data-structure.py
@@ -0,0 +1,17 @@
+# Set requires all the data elements to be unique
+
+tags = {
+  'python',
+  'coding',
+  'tutorials',
+  'coding'
+}
+
+print(tags)
+
+query_one = 'python' in tags   # will return True
+query_two = 'ruby' in tags   #will return False
+
+
+print(query_one)
+print(query_two)
