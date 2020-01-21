7538887c52251c39946fcabe825ab0ab6c929562
Phil Hawker <philhawker1211@gmail.com>
2020-01-21 11:23:51 -0700

    added comments

--- looping-over-ranges.py
+++ looping-over-ranges.py
@@ -1,5 +1,5 @@
-# for num in range(1, 10):
-#   print(num)
+for num in range(1, 10):   # (starting at 1, up to 10, but not including 10)
+  print(num)
 
 for num in range(1, 10, 2):   # (starting#, ending#, #s to skip)
   print(num)
