41ca7067889bc7c39d2d9ac1ef9f7ff1fc5f5259
Phil Hawker <philhawker1211@gmail.com>
2020-01-21 13:22:11 -0700

    for in loop list merging

--- for-in-loop-merge-lists.py
+++ for-in-loop-merge-lists.py
@@ -1,4 +1,11 @@
 legacy_customers = ['Alice' 'Bob']
 new_customers = ['Tiffany', 'Kristine']
 
+raw_db = [legacy_customers, new_customers]   #not what we want
 
+print(raw_db)   #not what we want
+
+for legacy_customer in legacy_customers:
+  new_customers.append(legacy_customer)
+
+print(new_customers)
