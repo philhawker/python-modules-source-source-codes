e23408170b07b2eccbc486f0e359dacfcfad0256
Phil Hawker <philhawker1211@gmail.com>
2020-01-21 16:02:43 -0700

    inital commit

---
+++ functions-basic-syntax.py
@@ -0,0 +1,9 @@
+def full_name(first, last):
+  print(f'{first} {last}')
+# Common convention to have 2 lines between the function and the call
+  
+full_name('Kelsey', 'Hawker')
+full_name('Phil', 'Hawker')
+full_name('Rick', 'Hawker')
+full_name('Cristi', 'Benedict')
+  
