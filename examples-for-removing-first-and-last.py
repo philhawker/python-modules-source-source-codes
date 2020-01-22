7c5e95f24718c07dacf87216cfa5d173098e1b95
Phil Hawker <philhawker1211@gmail.com>
2020-01-22 15:49:22 -0700

    initial commit

---
+++ examples-for-removing-first-and-last.py
@@ -0,0 +1,19 @@
+one, two, three = [1, 2, 3]
+
+one = 1
+two = 2
+three = 3
+
+one, *two, three = [1, 2, 3, 4, 5, 6]
+
+print(one)
+print(two)
+print(three)
+
+my_list = [1, 2, 3, 4, 5]
+# my_center = my_list[1:-1]
+# print(my_center)
+
+my_list.pop(0)
+my_list.pop(-1)
+print(my_list)
