9495441cc7ad05cbd38dbc189ad84b187a8982d4
Phil Hawker <philhawker1211@gmail.com>
2020-01-22 12:00:08 -0700

    added notations

--- lambdas-in-python.py
+++ lambdas-in-python.py
@@ -1,7 +1,11 @@
 # LAMBDA allows you to wrap up a smaller function and pass it to other functions. similar to a VARIABLE
 # functions are first class citizens. very mobile
 
-full_name = lambda first, last: f'{first} {last}'
+full_name = lambda first, last: f'{first} {last}'    #arguments first: then there values
+
+# This is the same as above the above LAMBDA
+def full_name(first, last):
+  return f'{first} {last}'
 
 
 def greeting(name):
